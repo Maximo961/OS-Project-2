9f2f983df02a598cb78f91dac4be27f0b6ab840d
Maximo <84199651+Maximo961@users.noreply.github.com>
2023-11-01 11:55:42 -0400

    Add files via upload

---
+++ userprog/pcbmanager.h
@@ -0,0 +1,30 @@
+#ifndef PCBMANAGER_H
+#define PCBMANAGER_H
+
+#include "bitmap.h"
+#include "pcb.h"
+#include "synch.h"
+class PCB;
+class Lock;
+class PCBManager {
+
+    public:
+        PCBManager(int maxProcesses);
+        ~PCBManager();
+
+        PCB* AllocatePCB();
+        int DeallocatePCB(PCB* pcb);
+        PCB* GetPCB(int pid);
+
+    private:
+        BitMap* bitmap;
+        PCB** pcbs;
+         Lock* pcbManagerLock;
+         int maxProcesses;
+        // Need a lock here
+        // Lock* pcbManagerLock;
+        
+         
+};
+
+#endif // PCBMANAGER_H
